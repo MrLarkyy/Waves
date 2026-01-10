103510ee4edc4efcf29a7edd2d27b41caa72ac87
MrLarkyy_ <studiotadikl@gmail.com>
2026-01-10 05:26:24 +0100

    Add new modules and update build configurations
    Added modules `Pakket`, `Execute`, and `Kommand` along with subprojects `Pakket:API` and `Pakket:NMS_1_21_9`. Updated build scripts to conditionally apply plugins and use dynamic project prefixes. Integrated `InputHandler` initialization on plugin enable.

--- src/main/kotlin/gg/aquatic/waves/Waves.kt
+++ src/main/kotlin/gg/aquatic/waves/Waves.kt
@@ -1,6 +1,16 @@
 package gg.aquatic.waves
 
+import gg.aquatic.waves.input.InputHandler
 import org.bukkit.plugin.java.JavaPlugin
 
 object Waves : JavaPlugin() {
+
+    override fun onLoad() {
+
+    }
+
+    override fun onEnable() {
+        InputHandler.initialize()
+    }
+
 }
