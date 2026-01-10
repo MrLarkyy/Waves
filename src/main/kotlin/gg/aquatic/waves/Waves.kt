687e85a86247aab45034a9995696c491d6f51e40
MrLarkyy_ <studiotadikl@gmail.com>
2026-01-10 19:34:23 +0100

    Initialize KMenu, Stacked, and Execute modules in `onEnable` method

--- src/main/kotlin/gg/aquatic/waves/Waves.kt
+++ src/main/kotlin/gg/aquatic/waves/Waves.kt
@@ -1,5 +1,9 @@
 package gg.aquatic.waves
 
+import gg.aquatic.execute.initExecute
+import gg.aquatic.kmenu.KMenu
+import gg.aquatic.kmenu.coroutine.KMenuCtx
+import gg.aquatic.stacked.initializeStacked
 import gg.aquatic.waves.input.InputHandler
 import org.bukkit.plugin.java.JavaPlugin
 
@@ -10,9 +14,10 @@ object Waves : JavaPlugin() {
     }
 
     override fun onEnable() {
+        KMenu.initialize()
+        initializeStacked(this, KMenuCtx.scope)
+        initExecute(this)
         InputHandler.initialize()
-
-
     }
 
 }
