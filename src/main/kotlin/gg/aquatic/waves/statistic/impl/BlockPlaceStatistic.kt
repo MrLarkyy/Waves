2f39666764252f59bb0240ccfd5053b58ec5751e
MrLarkyy_ <studiotadikl@gmail.com>
2026-01-20 05:20:52 +0100

    Add ListenerStatisticType abstraction for event-based statistics
    Introduces an abstract ListenerStatisticType class to simplify event-based statistic handling. Includes methods to create, register, and clean up event listeners, alongside a helper function for cleaner syntax in listener implementations.

--- src/main/kotlin/gg/aquatic/waves/statistic/impl/BlockPlaceStatistic.kt
+++ src/main/kotlin/gg/aquatic/waves/statistic/impl/BlockPlaceStatistic.kt
@@ -1,42 +1,30 @@
 package gg.aquatic.waves.statistic.impl
 
-import gg.aquatic.common.event
 import gg.aquatic.execute.argument.ObjectArgument
 import gg.aquatic.execute.argument.impl.PrimitiveObjectArgument
+import gg.aquatic.waves.statistic.ListenerStatisticType
 import gg.aquatic.waves.statistic.StatisticAddEvent
-import gg.aquatic.waves.statistic.StatisticType
 import org.bukkit.entity.Player
-import org.bukkit.event.HandlerList
-import org.bukkit.event.Listener
 import org.bukkit.event.block.BlockPlaceEvent
 
-object BlockPlaceStatistic: StatisticType<Player>() {
+object BlockPlaceStatistic: ListenerStatisticType<Player>() {
     override val arguments: Collection<ObjectArgument<*>> = listOf(
         PrimitiveObjectArgument("types", ArrayList<String>(), true)
     )
 
-    private var listener: Listener? = null
+    override fun createListener() = listen<BlockPlaceEvent> {
+        val player = it.player
 
-    override fun initialize() {
-        listener = event<BlockPlaceEvent>(ignoredCancelled = true) {
-            val player = it.player
+        for (statisticHandle in handles) {
+            val args = statisticHandle.args
+            val types = args.stringCollection("types") ?: listOf()
 
-            for (statisticHandle in handles) {
-                val args = statisticHandle.args
-                val types = args.stringCollection("types") ?: listOf()
-
-                if ("ALL" !in types && it.block.type.name.uppercase() !in types) {
-                    continue
-                }
-
-                val event = StatisticAddEvent(this, 1, player)
-                statisticHandle.consumer(event)
+            if ("ALL" !in types && it.block.type.name.uppercase() !in types) {
+                continue
             }
-        }
-    }
 
-    override fun terminate() {
-        listener?.let { HandlerList.unregisterAll(it) }
-        listener = null
+            val event = StatisticAddEvent(this, 1, player)
+            statisticHandle.consumer(event)
+        }
     }
 }
