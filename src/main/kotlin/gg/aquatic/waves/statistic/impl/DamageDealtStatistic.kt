1e4998652122bd7d480329e91eb503b2b5a0ccae
MrLarkyy_ <studiotadikl@gmail.com>
2026-01-20 05:20:54 +0100

    Add ListenerStatisticType abstraction for event-based statistics
    Introduces an abstract ListenerStatisticType class to simplify event-based statistic handling. Includes methods to create, register, and clean up event listeners, alongside a helper function for cleaner syntax in listener implementations.

--- src/main/kotlin/gg/aquatic/waves/statistic/impl/DamageDealtStatistic.kt
+++ src/main/kotlin/gg/aquatic/waves/statistic/impl/DamageDealtStatistic.kt
@@ -3,6 +3,7 @@ package gg.aquatic.waves.statistic.impl
 import gg.aquatic.common.event
 import gg.aquatic.execute.argument.ObjectArgument
 import gg.aquatic.execute.argument.impl.PrimitiveObjectArgument
+import gg.aquatic.waves.statistic.ListenerStatisticType
 import gg.aquatic.waves.statistic.StatisticAddEvent
 import gg.aquatic.waves.statistic.StatisticType
 import org.bukkit.entity.Player
@@ -10,32 +11,23 @@ import org.bukkit.event.HandlerList
 import org.bukkit.event.Listener
 import org.bukkit.event.entity.EntityDamageByEntityEvent
 
-object DamageDealtStatistic: StatisticType<Player>() {
+object DamageDealtStatistic: ListenerStatisticType<Player>() {
     override val arguments: Collection<ObjectArgument<*>> = listOf(
         PrimitiveObjectArgument("types", ArrayList<String>(), true)
     )
 
-    private var listener: Listener? = null
+    override fun createListener() = listen<EntityDamageByEntityEvent> {
+        val player = it.damager as? Player ?: return@listen
 
-    override fun initialize() {
-        listener = event<EntityDamageByEntityEvent>(ignoredCancelled = true) {
-            val player = it.damager as? Player ?: return@event
+        for (statisticHandle in handles) {
+            val args = statisticHandle.args
+            val types = args.stringCollection("types") ?: listOf()
 
-            for (statisticHandle in handles) {
-                val args = statisticHandle.args
-                val types = args.stringCollection("types") ?: listOf()
-
-                if ("ALL" !in types && it.entity.type.name.uppercase() !in types) {
-                    continue
-                }
-                val event = StatisticAddEvent(this, it.damage, player)
-                statisticHandle.consumer(event)
+            if ("ALL" !in types && it.entity.type.name.uppercase() !in types) {
+                continue
             }
+            val event = StatisticAddEvent(this, it.damage, player)
+            statisticHandle.consumer(event)
         }
-    }
-
-    override fun terminate() {
-        listener?.let { HandlerList.unregisterAll(it) }
-        listener = null
     }
 }
