ede1a74d828c9b5b23fa9a92d554207cd80c886d
MrLarkyy_ <studiotadikl@gmail.com>
2026-01-20 05:20:58 +0100

    Refactor DeathStatistic to extend ListenerStatisticType
    Replaced manual listener management with a cleaner ListenerStatisticType implementation. This simplifies the listener lifecycle and reduces boilerplate code while maintaining functionality for handling death statistics.

--- src/main/kotlin/gg/aquatic/waves/statistic/impl/DeathStatistic.kt
+++ src/main/kotlin/gg/aquatic/waves/statistic/impl/DeathStatistic.kt
@@ -2,6 +2,7 @@ package gg.aquatic.waves.statistic.impl
 
 import gg.aquatic.common.event
 import gg.aquatic.execute.argument.ObjectArgument
+import gg.aquatic.waves.statistic.ListenerStatisticType
 import gg.aquatic.waves.statistic.StatisticAddEvent
 import gg.aquatic.waves.statistic.StatisticType
 import org.bukkit.entity.Player
@@ -9,24 +10,16 @@ import org.bukkit.event.HandlerList
 import org.bukkit.event.Listener
 import org.bukkit.event.entity.PlayerDeathEvent
 
-object DeathStatistic: StatisticType<Player>() {
+object DeathStatistic: ListenerStatisticType<Player>() {
     override val arguments: Collection<ObjectArgument<*>> = listOf()
 
     private var listener: Listener? = null
+    override fun createListener() = listen<PlayerDeathEvent> {
+        val player = it.entity
 
-    override fun initialize() {
-        listener = event<PlayerDeathEvent>(ignoredCancelled = true) {
-            val player = it.entity
-
-            for (statisticHandle in handles) {
-                val event = StatisticAddEvent(this, 1, player)
-                statisticHandle.consumer(event)
-            }
+        for (statisticHandle in handles) {
+            val event = StatisticAddEvent(this, 1, player)
+            statisticHandle.consumer(event)
         }
-    }
-
-    override fun terminate() {
-        listener?.let { HandlerList.unregisterAll(it) }
-        listener = null
     }
 }
