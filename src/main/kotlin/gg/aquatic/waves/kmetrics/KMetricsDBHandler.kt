58d390205dd7a3f8963396fceba38cddca761c9a
MrLarkyy_ <studiotadikl@gmail.com>
2026-01-21 13:25:22 +0100

    Refactor `batchUpdate` and `cleanup` methods in `KMetricsDBHandler`
    Streamlined `batchUpdate` logic to use `onUpdate` in `upsert` operations, improving readability and efficiency. Enhanced `cleanup` method with improved period ID handling logic for better maintainability.

--- src/main/kotlin/gg/aquatic/waves/kmetrics/KMetricsDBHandler.kt
+++ src/main/kotlin/gg/aquatic/waves/kmetrics/KMetricsDBHandler.kt
@@ -4,6 +4,7 @@ import gg.aquatic.common.coroutine.VirtualsCtx
 import org.jetbrains.exposed.sql.Database
 import org.jetbrains.exposed.sql.SqlExpressionBuilder.eq
 import org.jetbrains.exposed.sql.SqlExpressionBuilder.notInList
+import org.jetbrains.exposed.sql.SqlExpressionBuilder.plus
 import org.jetbrains.exposed.sql.and
 import org.jetbrains.exposed.sql.deleteWhere
 import org.jetbrains.exposed.sql.transactions.experimental.newSuspendedTransaction
@@ -14,50 +15,55 @@ import java.util.*
 
 class KMetricsDBHandler(private val database: Database) {
 
-    suspend fun batchUpdate(updates: Map<String, Map<UUID, Pair<BigDecimal, String>>>) = newSuspendedTransaction(db = database, context = VirtualsCtx) {
-        if (updates.isEmpty()) return@newSuspendedTransaction
+    suspend fun batchUpdate(updates: Map<String, Map<UUID, Pair<BigDecimal, String>>>) =
+        newSuspendedTransaction(db = database, context = VirtualsCtx) {
+            if (updates.isEmpty()) return@newSuspendedTransaction
 
-        val now = LocalDateTime.now()
+            val now = LocalDateTime.now()
 
-        updates.forEach { (metricId, playerMap) ->
-            playerMap.forEach { (uuid, data) ->
-                val (increase, name) = data
+            updates.forEach { (metricId, playerMap) ->
+                playerMap.forEach { (uuid, data) ->
+                    val (increase, name) = data
 
-                for (type in PeriodType.entries) {
-                    val pId = type.getPeriodId(now)
+                    for (type in PeriodType.entries) {
+                        val pId = type.getPeriodId(now)
 
-                    KMetricsTable.upsert {
-                        it[KMetricsTable.metricId] = metricId
-                        it[KMetricsTable.binderUuid] = uuid
-                        it[KMetricsTable.periodType] = type
-                        it[KMetricsTable.periodId] = pId
-                        it[KMetricsTable.displayName] = name
-                        it[KMetricsTable.value] = increase
+                        KMetricsTable.upsert(onUpdate = {
+                            it[KMetricsTable.value] = KMetricsTable.value + increase; it[KMetricsTable.displayName] =
+                            name
+                        }) {
+                            it[KMetricsTable.metricId] = metricId
+                            it[KMetricsTable.binderUuid] = uuid
+                            it[KMetricsTable.periodType] = type
+                            it[KMetricsTable.periodId] = pId
+                            it[KMetricsTable.displayName] = name
+                            it[KMetricsTable.value] = increase
+                        }
                     }
                 }
             }
         }
-    }
 
-    suspend fun cleanup(type: PeriodType, amountToKeep: Int) = newSuspendedTransaction(db = database, context = VirtualsCtx) {
-        val now = LocalDateTime.now()
-        val idsToKeep = mutableListOf<String>()
+    suspend fun cleanup(type: PeriodType, amountToKeep: Int) =
+        newSuspendedTransaction(db = database, context = VirtualsCtx) {
+            val now = LocalDateTime.now()
+            val idsToKeep = mutableListOf<String>()
 
-        for (i in 0 until amountToKeep) {
-            val time = when (type) {
-                PeriodType.DAILY -> now.minusDays(i.toLong())
-                PeriodType.WEEKLY -> now.minusWeeks(i.toLong())
-                PeriodType.MONTHLY -> now.minusMonths(i.toLong())
-                PeriodType.ALLTIME -> now
+            for (i in 0 until amountToKeep) {
+                val time = when (type) {
+                    PeriodType.DAILY -> now.minusDays(i.toLong())
+                    PeriodType.WEEKLY -> now.minusWeeks(i.toLong())
+                    PeriodType.MONTHLY -> now.minusMonths(i.toLong())
+                    PeriodType.ALLTIME -> now
+                }
+                idsToKeep.add(type.getPeriodId(time))
             }
-            idsToKeep.add(type.getPeriodId(time))
-        }
 
-        KMetricsTable.deleteWhere {
-            (KMetricsTable.periodType eq type) and
-                    (KMetricsTable.periodId notInList idsToKeep)
+            KMetricsTable.deleteWhere {
+                (KMetricsTable.periodType eq type) and
+                        (KMetricsTable.periodId notInList idsToKeep)
+            }
         }
-    }
 
     suspend fun getValue(metricId: String, uuid: UUID, type: PeriodType, pId: String): BigDecimal =
         newSuspendedTransaction(db = database, context = VirtualsCtx) {
